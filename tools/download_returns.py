2044242aa5f3e04a07937ded930dcd6d5a765684
Jannic Cutura <Janniccutura@gmail.com>
2021-01-31 12:14:12 +0100

    fixed downloading from S3

--- tools/download_returns.py
+++ tools/download_returns.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-
-fund_returns = pd.read_excel("https://buy-and-hold-strategy.s3.eu-central-1.amazonaws.com/fund_returns.xlsx")
+fund_returns = pd.read_excel("https://buy-and-hold-strategy.s3.eu-central-1.amazonaws.com/fund_returns.xlsx",
+                             engine='openpyxl')
 
 
 
