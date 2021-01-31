b239008e39ebe2a38b203b8e4ccae399aa370a33
Jannic Cutura <Janniccutura@gmail.com>
2021-01-31 12:10:44 +0100

    added fownloading

--- tools/download_returns.py
+++ tools/download_returns.py
@@ -1,19 +1,11 @@
 import pandas as pd
-import yfinance as yf
-
-from tools.funds import funds_df
-
-tsla_df = yf.download('IE00B2PC0609',
-                      start='2019-01-01',
-                      end='2019-12-31',
-                      progress=False)
-tsla_df.head()
 
 
-df = investpy.get_fund_recent_data(fund='Dimensional Global Targeted Value Fund Fonds', country="germany")
-print(df.head())
+fund_returns = pd.read_excel("https://buy-and-hold-strategy.s3.eu-central-1.amazonaws.com/fund_returns.xlsx")
 
-funds_dict = pd.DataFrame(investpy.get_funds_dict(country=None)).query("isin == 'IE00B2PC0609'")
+
+
+
 
 
 
